f1eea1a550e4eba5948ad72cdd1b962e5d2e55c8
radamantes <diego.brito@hotmail.com.br>
2017-07-08 09:18:48 -0500

    Exercicios de Fatorial

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Escreva um programa que calcule o fatorial de 10.Regra: FATORIAL DE 5 = 5= 5*4*3*2*1
+
+      int fat = 10;
+
+      for (int cont = 1; cont < 10; cont++)
+      {
+        fat *= cont;
+        Console.WriteLine(fat);
+      }
+      Console.WriteLine("O Fatorial de 10 é: "+ fat);
+
+    }
+  }
+}
