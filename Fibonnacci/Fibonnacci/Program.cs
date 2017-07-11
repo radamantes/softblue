106f6eb22ba9531a1bf92e00a3589eb07d54aa7d
radamantes <diego.brito@hotmail.com.br>
2017-07-10 22:33:01 -0500

    Fibonnacci

---
+++ Fibonnacci/Fibonnacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonnacci
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a, b;
+
+      int temp = 0;
+
+      a = 1;
+
+      b = 1;
+      
+      for (int cont = 0; cont < 15; cont++)
+      {
+
+        Console.WriteLine(b);
+
+        a = b;
+
+        b += temp;
+
+        temp = a;
+      }
+
+      
+    }
+  }
+}
