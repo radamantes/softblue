78bde2786be61e536ccb732a438849aa0b7de5d3
radamantes <diego.brito@hotmail.com.br>
2017-07-13 13:30:41 -0500

    Adicioado Exercicio Conta de Banco

---
+++ ClassesObjetos/ClassesObjetos/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+  class Conta
+  {
+    public string nome;
+    public int saldo;
+    public int numeroConta;
+
+    public void Sacar(int vpSaque)
+    {
+      saldo -= vpSaque;
+    }
+
+    public int Saldo()
+    {
+      return saldo;
+    }
+
+    public void Depositar(int valor)
+    {
+      saldo += valor;
+    }
+
+    public void Transferir(int vpTranferencia, Conta contraTransferida)
+    {
+      Sacar(vpTranferencia);
+      contraTransferida.Depositar(vpTranferencia); //RECEBER O VALOR A SER TRANSFERIDO E ACRESCENTA EM SUA CONTA
+
+    }
+  }
+
+}
---
+++ ClassesObjetos/ClassesObjetos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //CONTA NÚMERO 1
+      Conta c1 = new Conta();
+      c1.nome = "Diego";
+      c1.saldo = 1500;
+      c1.numeroConta = 159;
+
+      Console.WriteLine("Nome do Cliente 1: " + c1.nome);
+      Console.WriteLine("Saldo atual: " + c1.saldo);
+
+
+      //-------------------------------------------------------
+
+      //CONTA NÚMERO 2
+      Conta c2 = new Conta();
+      c2.nome = "Raquel";
+      c2.saldo = 700;
+      c2.numeroConta = 741;
+
+
+      Console.WriteLine("Nome do Cliente 2: " + c2.nome);
+      Console.WriteLine("Saldo atual: " + c2.saldo);
+
+      int valorParaSacar = 200; //VALOR PARA SAQUE
+      int valorParaTransferir = 125; //VALOR PARA TRANSFERENCIA
+
+                 
+
+      c1.Sacar(valorParaSacar);
+      c1.Transferir(valorParaTransferir, c2);
+          
+      Console.WriteLine(c1.nome + " Sacou: " + valorParaSacar);
+   
+      Console.WriteLine(c1.nome + " Transferiu " +  valorParaTransferir + " para " + c2.nome);
+      
+      Console.WriteLine("Restou na conta de " + c1.nome + " " + c1.Saldo());
+
+      Console.WriteLine("Restou na conta de " + c2.nome + " " + c2.Saldo());
+
+      
+
+    }
+  }
+}
