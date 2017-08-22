80e98ddbff31ae6683b9fb1f37482f5c0ad74e84
radamantes <diego.brito@hotmail.com.br>
2017-08-22 17:39:24 -0500

    Exercício conta bancária feito com várias exceções

---
+++ BancariaException/BancariaException/SaldoInvalidoException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancariaException
+{
+  // Exceção que representa uma quantidade de saldo insuficiente na conta para realizar a transação
+  class SaldoInsuficienteException : Exception
+  {
+    // Saldo total disponível na conta
+    private double saldoDisponivel;
+
+    // Construtor. Recebe uma mensagem de erro e o saldo disponível
+    public SaldoInsuficienteException(String message, double saldoDisponivel)
+        : base(message)
+    {
+      this.saldoDisponivel = saldoDisponivel;
+    }
+
+    // Obtém o saldo disponível
+    public double SaldoDisponivel
+    {
+      get { return saldoDisponivel; }
+    }
+  }
+}
+
---
+++ BancariaException/BancariaException/ValorInvalidoException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancariaException
+{
+
+  public class ValorInvalidoException : Exception
+  {
+    private double valorInvalido;
+
+
+    //CONSTRUTORES
+    public ValorInvalidoException() { }
+    public ValorInvalidoException(string message, double valor) : base(message)
+    {
+      this.valorInvalido = valor;
+    }
+    public ValorInvalidoException(string message, Exception inner) : base(message, inner) { }
+
+
+    //PROPRIEDADE LEITURA
+    public double ValorInvalido
+    {
+      get { return valorInvalido; }
+    }
+  }
+}
