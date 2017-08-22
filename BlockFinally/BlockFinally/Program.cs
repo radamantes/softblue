d8bc84dc286f9d01cf6f3a7d689ab46edb740882
radamantes <diego.brito@hotmail.com.br>
2017-08-22 17:37:02 -0500

    Exercicio com Block Finally

---
+++ BlockFinally/BlockFinally/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockFinally
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Executar(true); // QUANDO MUDA PARA TRUE GERA EXCEÇÃO
+       
+      }
+      catch (ArgumentException e)
+      {
+        Console.WriteLine("Exceção tratada");
+      }
+      finally // ELE É SEMPRE EXECUTANDO INDEPENDE SE EXISTE EXCEÇÕES OU NÃO
+      {
+        Console.WriteLine("Aplicação terminada");
+      }
+
+    }
+
+    static void Executar(bool b)
+    {
+      if (b)
+      {
+        throw new ArgumentException();
+      }
+    }
+  }
+}
