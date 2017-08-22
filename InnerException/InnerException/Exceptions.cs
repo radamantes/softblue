944e63e5b8e2e61105f68fdf41a9ea0a935bf9b5
radamantes <diego.brito@hotmail.com.br>
2017-08-22 17:37:56 -0500

    Exercício que ensina inner Exception

---
+++ InnerException/InnerException/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerException
+{
+
+  public class DivisaoException : Exception
+  {
+    public DivisaoException() { }
+    public DivisaoException(string message) : base(message) { }
+    public DivisaoException(string message, Exception inner) : base(message, inner) { }
+  }
+
+  public class CaculoException : Exception
+  {
+    public CaculoException() { }
+    public CaculoException(string message) : base(message) { }
+    public CaculoException(string message, Exception inner) : base(message, inner) { }
+  }
+}
---
+++ InnerException/InnerException/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerException
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Calculador c = new Calculador();
+      double result = c.Dividir(10, 0);
+      Console.WriteLine(result);
+    }
+  }
+
+  class Calculador
+  {
+    public double Dividir(double a, double b)
+    {
+
+      try
+      {
+        Divisao d = new Divisao(a, b);
+        return d.Dividir();
+      }
+      catch (DivisaoException e)
+      {
+        throw new CaculoException("Erro de Cálculo", e);
+      }
+
+    }
+  }
+
+  class Divisao
+  {
+    public double Numerador { get; set; }
+    public double Denominador { get; set; }
+
+    public Divisao(double numerador, double denominador)
+    {
+      this.Numerador = numerador;
+      this.Denominador = denominador;
+    }
+
+    public double Dividir()
+    {
+      if (Denominador == 0)
+      {
+        throw new DivisaoException("Denominador não pode ser zero");
+      }
+      return Numerador / Denominador;
+    }
+  }
+}
