1bd88733177a5a45ebb539c98ee06eb1d803dce7
radamantes <diego.brito@hotmail.com.br>
2017-08-31 12:52:04 -0500

    Criando Método Genéricos

---
+++ GenericsMetodos/GenericsMetodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMetodos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      int s = Creator.Create<int>();
+    }
+  }
+
+  class Creator
+  {
+    public static T Create<T>() where T : new()
+    {
+      return new T();
+    }
+  }
+}
