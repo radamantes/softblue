256ab2344dc5d898620f5837a315c6b9a0647722
radamantes <diego.brito@hotmail.com.br>
2017-10-15 07:29:09 -0500

    Converter uma Lista utilizando lambda de for simplificada

---
+++ ListConvertLambda/ListConvertLambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListConvertLambda
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<double> lista = new List<double>() { 5, 7, 3, 2, 6, 18 };
+
+      List<double> newList = lista.ConvertAll(x => x / 2);
+
+      newList.ForEach(e => Console.WriteLine(e));
+
+    }
+  }
+}
